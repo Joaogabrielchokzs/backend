d55b01b6187462921a2ff0afd2b0b29e4c32627d
joaogabrielchokzs <ra56628154882@acaonsfatima.org.br>
2024-11-04 16:16:50 -0300

    api teste

--- backend/src/utils/rotas.js
+++
@@ -1,7 +0,0 @@
-import clienteController from'././../backend/src/Controller/clienteControlller.js'
-import pedidoController from'././../backend/src/Controller/pedidoController.js'
-
-export default function adicionarRotas(servidor) {
-    servidor.use(clienteController);
-    servidor.use(pedidoController);
-}
